6da39335e214e88a4b23603d0bcbc9ed7dd2b9a0
drbastasl <drbastiasl@gmail.com>
2022-06-24 18:54:08 -0400

    New interface

--- main.py
+++ main.py
@@ -4,39 +4,67 @@ from datetime import date
 import pandas as pd 
 import requests
 from bs4 import BeautifulSoup
+from rich import print
 
-warnings.filterwarnings("ignore")
+print('Bienvenido al magic-bot-pasta de Jew y Max:\n')
+print('Porfavor, elige tu opcion:')
+print('[bold bright_cyan][1][/bold bright_cyan] Revisar todas las sheets')
+print('[bold bright_cyan][2][/bold bright_cyan] Revisar solo la sheet Carpeta')
 
-df_ori = pd.read_excel('Magic.xlsx', sheet_name = 'Carpeta')
-df_cartas = df_ori['Carta']
-df = df_ori.loc[:, 'Link'].to_list()
+opcion = input('Elija su opcion: ')
 
+while True:
+    if opcion == '1':
+        Shiits = ['Carpeta', 'Arcades', 'MBT', 'Coleccion','Bulk', 'BulkFoil', 'NivPauper']
+        break
+    elif opcion == '2':
+        shit = input('Ingrese el nombre del sheet a escanear: ')
+        Shiits = []
+        Shiits.append(shit)
+        break
+    else:
+        print('[bold red][ERROR][/bold red] Opcion elegida invalida, porfavor elige una opcion valida')
+        opcion = input('Elija su opcion: ')
 
-with requests.Session() as s:
-    precios = []
-    i = 0
-    for item in df:
-        page = s.get(item, headers={"User-Agent":"Mozilla/5.0"})
-        soup = BeautifulSoup(page.content, 'html.parser')
-        if 'cardkingdom' in item:
-            element = soup.find(class_="stylePrice").get_text()
-        else:
-            if soup.find(class_="price price--non-sale").get_text() != "\n":
-                element = soup.find(class_="price price--non-sale").get_text()
+for item in Shiits:
+    warnings.filterwarnings("ignore")
+    sheetname = item
+    df_hist = pd.read_excel(f'Historico{sheetname}.xlsx')
+    df_ori = pd.read_excel('Magic.xlsx', sheet_name = sheetname)
+    df_precios = df_ori['Precio SCG']
+    df_cartas = df_ori['Carta']
+    df = df_ori.loc[:, 'Link'].to_list()
+
+    with requests.Session() as s:
+        precios = []
+        i = 0
+        for item in df:
+            page = s.get(item, headers={"User-Agent":"Mozilla/5.0"})
+            soup = BeautifulSoup(page.content, 'html.parser')
+            if 'cardkingdom' in item:
+                element = soup.find(class_="stylePrice").get_text()
             else:
-                element = soup.find(class_="price price--withoutTax").get_text()
-        element = element.replace('$', '')
-        element = float(element)
-        if '(PL)' in df_cartas[i]:
-            print('Ta usada la wea')
-            element = element * 0.80
-        if '(HP)' in df_cartas[i]:
-            print('Ta MUY usada la wea')
-            element = element * 0.33
-        precios.append(element)
-        print(f'{df_cartas[i]} - {element}')
-        i += 1
+                if soup.find(class_="price price--non-sale").get_text() != "\n":
+                    element = soup.find(class_="price price--non-sale").get_text()
+                else:
+                    element = soup.find(class_="price price--withoutTax").get_text()
+            element = element.replace('$', '')
+            element = float(element)
+            if '(PL)' in df_cartas[i]:
+                print('Ta usada la wea')
+                element = element * 0.80
+            if '(HP)' in df_cartas[i]:
+                print('Ta MUY usada la wea')
+                element = element * 0.33
+            precios.append(element)
+            print(f'{df_cartas[i]} - {element}')
+            i += 1
 
-df_final = pd.DataFrame({'Cartas': df_cartas,'Precio': precios})
-today = date.today()
-df_final.to_excel(f'PreciosActualizados{today}.xlsx')
+    df_final = pd.DataFrame({'Cartas': df_cartas,'Precio': precios, 'PrecioAnt': df_precios, 'Diferencia': precios - df_precios})
+    today = date.today()
+    PrecioHist = 'Precio' + today.strftime("%Y-%m-%d")
+    # df_hist.assign(PrecioHist = precios)
+    df_hist[PrecioHist] = precios
+    df_final.to_excel(f'PreciosActualizados{sheetname}{today}.xlsx')
+    df_hist.to_excel(f'Historico{sheetname}.xlsx')
+    print('[bold bright_cyan]El Bot a terminado <3[/bold bright_cyan]')
